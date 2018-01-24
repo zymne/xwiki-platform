024eebd985fe6592bf048db30664c4a5d164b23f
Vincent Massol <vincent@massol.net>
2018-01-24 16:41:12 +0100

    XWIKI-14929: Run functional tests inside Docker containers XWIKI-14930: Generate and distribute official Docker images

--- xwiki-platform-distribution/xwiki-platform-distribution-docker/xwiki-platform-distribution-docker-tomcat-mysql/src/main/docker/xwiki/docker-entrypoint.sh
+++ xwiki-platform-distribution/xwiki-platform-distribution-docker/xwiki-platform-distribution-docker-tomcat-mysql/src/main/docker/xwiki/docker-entrypoint.sh
@@ -33,31 +33,24 @@ function other_starts() {
   restoreConfigurationFile 'xwiki.properties'
 }
 
-# $1 - the path to xwiki.[cfg|properties]
-# $2 - the setting/property to set
-# $3 - the new value
-function xwiki_replace() {
-  sed -i s~"\#\? \?$2 \?=.*"~"$2=$3"~g "$1"
+# $1 - the line to add to the end of the xwiki.cfg file
+function xwiki_add_cfg() {
+    printf "    Adding: ${1}\n"
+    printf "${1}\n" >> /usr/local/tomcat/webapps/ROOT/WEB-INF/xwiki.cfg
 }
 
-# $1 - the setting/property to set
-# $2 - the new value
-function xwiki_set_cfg() {
-  xwiki_replace /usr/local/tomcat/webapps/ROOT/WEB-INF/xwiki.cfg "$1" "$2"
+# $1 - the line to add to the end of the xwiki.properties file
+function xwiki_add_properties() {
+    printf "    Adding: ${1}\n"
+    printf "${1}\n" >> /usr/local/tomcat/webapps/ROOT/WEB-INF/xwiki.properties
 }
 
-# $1 - the setting/property to set
-# $2 - the new value
-function xwiki_set_properties() {
-  xwiki_replace /usr/local/tomcat/webapps/ROOT/WEB-INF/xwiki.properties "$1" "$2"
-}
-
-# Allows to use sed but with user input which can contain special sed characters such as \, / or &.
+# Allows to use sed but with user input which can contain special sed characters such as \, / or &. Also supports tab.
 # $1 - the text to search for
 # $2 - the replacement text
 # $3 - the file in which to do the search/replace
 function safesed {
-  sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
+    sed -i "s/$(echo "$1" | sed -e 's/\([[\/.*]\|\]\)/\\&/g' | sed -e 's:\t:\\t:g')/$(echo "$2" | sed -e 's/[\/&]/\\&/g' | sed -e 's:\t:\\t:g')/g" "$3"
 }
 
 # $1 - the config file name found in WEB-INF (e.g. "xwiki.cfg")
@@ -88,36 +81,42 @@ function configure() {
   # Note that we always provide default values when no value is passed
 
   echo 'Configuring XWiki...'
+
+  echo '  Updating hibernate.cfg.xml...'
+
   safesed "@replaceuser@" ${DB_USER:-xwiki} /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
   safesed "@replacepassword@" ${DB_PASSWORD:-xwiki} /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
   safesed "@replacecontainer@" ${DB_HOST:-db} /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
   safesed "@replacedatabase@" ${DB_DATABASE:-xwiki} /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
-  safesed "@replaceextra@" ""${DB_EXTRA_:-}"" /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
+  safesed "@replaceextra@" "${DB_EXTRA:-}" /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
 
-  echo '  Using filesystem-based attachments...'
-  xwiki_set_cfg 'xwiki.store.attachment.hint' 'file'
-  xwiki_set_cfg 'xwiki.store.attachment.versioning.hint' 'file'
-  xwiki_set_cfg 'xwiki.store.attachment.recyclebin.hint' 'file'
-  echo '  Generating authentication validation and encryption keys...'
-  xwiki_set_cfg 'xwiki.authentication.validationKey' "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-  xwiki_set_cfg 'xwiki.authentication.encryptionKey' "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-  if [ ! -z ${PLUGINS+x} ]; then
-    echo '  Setting custom plugin list...'
-    xwiki_set_cfg 'xwiki.plugins' "${PLUGINS}"
-  fi
-  if [ ! -z ${SUPERADMIN_PWD+x} ]; then
-    echo '  Setting superadmin password...'
-    xwiki_set_cfg 'xwiki.superadminpassword' "${SUPERADMIN_PWD}"
+  echo '  Updating xwiki.cfg...'
+
+  ## Using filesystem-based attachments
+  xwiki_add_cfg 'xwiki.store.attachment.hint = file'
+  xwiki_add_cfg 'xwiki.store.attachment.versioning.hint = file'
+  xwiki_add_cfg 'xwiki.store.attachment.recyclebin.hint = file'
+
+  ## Generating authentication validation and encryption keys
+  xwiki_add_cfg "xwiki.authentication.validationKey = $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+  xwiki_add_cfg "xwiki.authentication.encryptionKey = $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+
+  ## Adding extra properties passed by the user
+  if [ ! -z ${EXTRA_CFG+x} ]; then
+    xwiki_add_cfg "${EXTRA_CFG}"
   fi
 
-  echo '  Setting permanent directory...'
-  xwiki_set_properties 'environment.permanentDirectory' '/usr/local/xwiki/data'
-  echo '  Configure libreoffice...'
-  xwiki_set_properties 'openoffice.autoStart' 'true'
+  echo '  Updating xwiki.properties...'
+
+  ## Setting permanent directory
+  xwiki_add_properties 'environment.permanentDirectory = /usr/local/xwiki/data'
+
+  ## Start libreoffice automatically
+  xwiki_add_properties 'openoffice.autoStart = true'
+
+  ## Adding extra properties passed by the user
   if [ ! -z ${EXTRA_PROPERTIES+x} ]; then
-    echo '  Adding the following additional properties:'
-    printf "${EXTRA_PROPERTIES}\n"
-    printf "${EXTRA_PROPERTIES}\n" >> /usr/local/tomcat/webapps/ROOT/WEB-INF/xwiki.properties
+    xwiki_add_properties "${EXTRA_PROPERTIES}"
   fi
 
   # If the files already exist then copy them to the XWiki's WEB-INF directory. Otherwise copy the default config
